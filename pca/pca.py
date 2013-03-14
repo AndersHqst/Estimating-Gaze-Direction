5aea11506246695f5e5f2010a1d9451432da5edf
David Thomas <davidat@gmail.com>
2013-03-14 17:50:44 +0100

    method extract

--- pca/pca.py
+++ pca/pca.py
@@ -3,7 +3,7 @@ import scipy.io
 import matplotlib.pyplot as plt
 
 
-# functions
+# functions ----
 
 def featureNormalize(data):
     ''' Normalizes data to a mean value of 0 and a standard deviation of 1 '''
@@ -19,29 +19,31 @@ def getCovarianceMatrix(normalizedData):
     m = normalizedData.shape[0]
     transposed = np.transpose(normalizedData)
     return transposed.dot(normalizedData) / m
-    pass
 
-# flow
+def plotData(data, u, s, v):
+    plt.hold('on')
+    plt.plot(data[:,0], data[:,1], 'bo')
+    
+    p1, p2 = mean, 1.5 * s[0] * np.transpose(u[:,0]) # ved ikke lige hvad de 1.5 laver, antager det er for plottets skyld
+    p3, p4 = mean, 1.5 * s[1] * np.transpose(u[:,1])
+    plt.arrow(p1[0], p1[1], p2[0], p2[1])
+    plt.arrow(p3[0], p3[1], p4[0], p4[1])
+    
+    plt.axis([0.5, 6.5, 2, 8])
+    plt.show()
+
+
+# flow ----
 
 data = scipy.io.loadmat('ex7data1.mat')
 data = data['X']
 
 normalizedData, mean, variance = featureNormalize(data)
 covarianceMatrix = getCovarianceMatrix(normalizedData)
-u, s, v = np.linalg.svd(covarianceMatrix)
 
-plt.hold('on')
-plt.plot(data[:,0], data[:,1], 'bo')
-plt.figure
+u, s, v = np.linalg.svd(covarianceMatrix) # numpy giver et s-array hvor kun diagonalerne fra S-matrixen beskrevet i kurset er med
 
-p1, p2 = mean, 1.5 * s[0] * np.transpose(u[:,0])
-p3, p4 = mean, 1.5 * s[1] * np.transpose(u[:,1])
-plt.arrow(p1[0], p1[1], p2[0], p2[1])
-plt.arrow(p3[0], p3[1], p4[0], p4[1])
-
-
-plt.axis([0.5, 6.5, 2, 8])
-plt.show()
+plotData(data, u, s, v)
 
 
 raw_input()
